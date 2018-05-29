3882ad65b36f28f59e7acdb8092ddbed2b65cb37
younne0915 <younne@163.com>
2018-05-30 07:21:05 +0800

    Initial commit

---
+++ Server/Hotfix/Module/Message/InnerMessageDispatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ETModel;
+
+namespace ETHotfix
+{
+	public class InnerMessageDispatcher: IMessageDispatcher
+	{
+		public void Dispatch(Session session, Packet packet)
+		{
+			ushort opcode = packet.Opcode;
+			Type messageType = Game.Scene.GetComponent<OpcodeTypeComponent>().GetType(opcode);
+			IMessage message = (IMessage)session.Network.MessagePacker.DeserializeFrom(messageType, packet.Bytes, Packet.Index, packet.Length - Packet.Index);
+			
+			// 收到actor消息,放入actor队列
+			if (message is IActorMessage iActorMessage)
+			{
+				Entity entity = (Entity)Game.EventSystem.Get(iActorMessage.ActorId);
+				if (entity == null)
+				{
+					Log.Warning($"not found actor: {iActorMessage.ActorId}");
+					ActorResponse response = new ActorResponse
+					{
+						Error = ErrorCode.ERR_NotFoundActor,
+						RpcId = iActorMessage.RpcId
+					};
+					session.Reply(response);
+					return;
+				}
+
+				MailBoxComponent mailBoxComponent = entity.GetComponent<MailBoxComponent>();
+				if (mailBoxComponent == null)
+				{
+					ActorResponse response = new ActorResponse
+					{
+						Error = ErrorCode.ERR_ActorNoActorComponent,
+						RpcId = iActorMessage.RpcId
+					};
+					session.Reply(response);
+					Log.Error($"actor没有挂载ActorComponent组件: {entity.GetType().Name} {entity.Id}");
+					return;
+				}
+				
+				mailBoxComponent.Add(new ActorMessageInfo() { Session = session, Message = iActorMessage });
+				return;
+			}
+			
+			Game.Scene.GetComponent<MessageDispatherComponent>().Handle(session, new MessageInfo(opcode, message));
+		}
+	}
+}
---
+++ Unity/Assets/T4M/Shaders/PreviewT4M.shader
@@ -0,0 +1,26 @@
+﻿Shader "T4MShaders/PreviewT4M"
+{
+	Properties
+	{
+		_Transp("Transparency", Range(0,1)) = 1
+		_MainTex("Texture", 2D) = "" { }
+		_MaskTex("Mask (RGB) Trans (A)", 2D) = ""
+		{
+			TexGen ObjectLinear
+		}
+	}
+
+	SubShader
+	{
+		Pass{
+			Blend SrcAlpha OneMinusSrcAlpha
+			SetTexture[_MainTex]
+			SetTexture[_MaskTex]
+		{
+		constantColor(1,1,1,[_Transp])
+		combine previous , texture* constant
+		Matrix[_Projector]
+		}
+	}
+	}
+}
