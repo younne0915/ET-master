8323a7ad3af612f80711afc154c458456bc2102f
younne0915 <younne@163.com>
2018-06-12 07:40:06 +0800

    *[] config

--- Server/Model/Module/Config/ConfigHelper.cs
+++ Server/Model/Module/Config/ConfigHelper.cs
@@ -7,7 +7,7 @@ namespace ETModel
 	{
 		public static string GetText(string key)
 		{
-			string path = $"../Config/{key}.txt";
+			string path = $"./Config/{key}.txt";
 			try
 			{
 				string configStr = File.ReadAllText(path);
