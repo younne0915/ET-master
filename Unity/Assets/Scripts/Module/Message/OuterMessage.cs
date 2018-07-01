fd65cf76da6ff02f9dbabdd78a5c06b763f07620
younne0915 <younne@163.com>
2018-07-01 17:29:00 +0800

    *[]heroID

--- Unity/Assets/Scripts/Module/Message/OuterMessage.cs
+++ Unity/Assets/Scripts/Module/Message/OuterMessage.cs
@@ -2,213 +2,228 @@ using ProtoBuf;
 using ETModel;
 using System.Collections.Generic;
 using MongoDB.Bson.Serialization.Attributes;
-namespace ETModel
+namespace ETHotfix
 {
-	[Message(OuterOpcode.Actor_Test)]
-	[ProtoContract]
-	public partial class Actor_Test: IActorLanuch
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.Actor_Test)]
+    [ProtoContract]
+    public partial class Actor_Test : IActorNotify
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(93, IsRequired = true)]
-		public long ActorId { get; set; }
+        [ProtoMember(93, IsRequired = true)]
+        public long ActorId { get; set; }
 
-		[ProtoMember(1, IsRequired = true)]
-		public string Info;
+        [ProtoMember(1, IsRequired = true)]
+        public string Info;
 
-	}
+    }
 
-	[Message(OuterOpcode.Actor_TestRequest)]
-	[ProtoContract]
-	public partial class Actor_TestRequest: IActorRequest
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.Actor_TestRequest)]
+    [ProtoContract]
+    public partial class Actor_TestRequest : IActorRequest
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(93, IsRequired = true)]
-		public long ActorId { get; set; }
+        [ProtoMember(93, IsRequired = true)]
+        public long ActorId { get; set; }
 
-		[ProtoMember(1, IsRequired = true)]
-		public string request;
+        [ProtoMember(1, IsRequired = true)]
+        public string request;
 
-	}
+    }
 
-	[Message(OuterOpcode.Actor_TestResponse)]
-	[ProtoContract]
-	public partial class Actor_TestResponse: IActorResponse
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.Actor_TestResponse)]
+    [ProtoContract]
+    public partial class Actor_TestResponse : IActorResponse
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(91, IsRequired = true)]
-		public int Error { get; set; }
+        [ProtoMember(91, IsRequired = true)]
+        public int Error { get; set; }
 
-		[ProtoMember(92, IsRequired = true)]
-		public string Message { get; set; }
+        [ProtoMember(92, IsRequired = true)]
+        public string Message { get; set; }
 
-		[ProtoMember(1, IsRequired = true)]
-		public string response;
+        [ProtoMember(1, IsRequired = true)]
+        public string response;
 
-	}
+    }
 
-	[Message(OuterOpcode.Actor_TransferRequest)]
-	[ProtoContract]
-	public partial class Actor_TransferRequest: IActorRequest
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.Actor_TransferRequest)]
+    [ProtoContract]
+    public partial class Actor_TransferRequest : IActorRequest
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(93, IsRequired = true)]
-		public long ActorId { get; set; }
+        [ProtoMember(93, IsRequired = true)]
+        public long ActorId { get; set; }
 
-		[ProtoMember(1, IsRequired = true)]
-		public int MapIndex;
+        [ProtoMember(1, IsRequired = true)]
+        public int MapIndex;
 
-	}
+    }
 
-	[Message(OuterOpcode.Actor_TransferResponse)]
-	[ProtoContract]
-	public partial class Actor_TransferResponse: IActorResponse
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.Actor_TransferResponse)]
+    [ProtoContract]
+    public partial class Actor_TransferResponse : IActorResponse
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(91, IsRequired = true)]
-		public int Error { get; set; }
+        [ProtoMember(91, IsRequired = true)]
+        public int Error { get; set; }
 
-		[ProtoMember(92, IsRequired = true)]
-		public string Message { get; set; }
+        [ProtoMember(92, IsRequired = true)]
+        public string Message { get; set; }
 
-	}
+    }
 
-	[Message(OuterOpcode.C2G_EnterMap)]
-	[ProtoContract]
-	public partial class C2G_EnterMap: IRequest
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.C2G_EnterMap)]
+    [ProtoContract]
+    public partial class C2G_EnterMap : IRequest
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-	}
+    }
 
-	[Message(OuterOpcode.G2C_EnterMap)]
-	[ProtoContract]
-	public partial class G2C_EnterMap: IResponse
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.G2C_EnterMap)]
+    [ProtoContract]
+    public partial class G2C_EnterMap : IResponse
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(91, IsRequired = true)]
-		public int Error { get; set; }
+        [ProtoMember(91, IsRequired = true)]
+        public int Error { get; set; }
 
-		[ProtoMember(92, IsRequired = true)]
-		public string Message { get; set; }
+        [ProtoMember(92, IsRequired = true)]
+        public string Message { get; set; }
 
-		[ProtoMember(1, IsRequired = true)]
-		public long UnitId;
+        [ProtoMember(1, IsRequired = true)]
+        public long UnitId;
 
-		[ProtoMember(2, IsRequired = true)]
-		public int Count;
+        [ProtoMember(2, IsRequired = true)]
+        public int Count;
 
-	}
+    }
 
-	[Message(OuterOpcode.UnitInfo)]
-	[ProtoContract]
-	public partial class UnitInfo
-	{
-		[ProtoMember(1, IsRequired = true)]
-		public long UnitId;
+    [Message(OuterOpcode.UnitInfo)]
+    [ProtoContract]
+    public partial class UnitInfo
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public long UnitId;
 
-		[ProtoMember(2, IsRequired = true)]
-		public int X;
+        [ProtoMember(2, IsRequired = true)]
+        public int X;
 
-		[ProtoMember(3, IsRequired = true)]
-		public int Z;
+        [ProtoMember(3, IsRequired = true)]
+        public int Z;
 
-	}
+        [ProtoMember(4, IsRequired = true)]
+        public int heroID;
 
-	[Message(OuterOpcode.Actor_CreateUnits)]
-	[ProtoContract]
-	public partial class Actor_CreateUnits: IActorLanuch
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+        public UnitInfo()
+        {
 
-		[ProtoMember(93, IsRequired = true)]
-		public long ActorId { get; set; }
+        }
 
-		[ProtoMember(1, TypeName = "ETModel.UnitInfo")]
-		public List<UnitInfo> Units = new List<UnitInfo>();
+        public UnitInfo(long uID, int x, int z, int hID)
+        {
+            this.UnitId = uID;
+            this.X = x;
+            this.Z = z;
+            this.heroID = hID;
+        }
+    }
 
-	}
+    [Message(OuterOpcode.Actor_CreateUnits)]
+    [ProtoContract]
+    public partial class Actor_CreateUnits : IActorNotify
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-	[Message(OuterOpcode.Frame_ClickMap)]
-	[ProtoContract]
-	public partial class Frame_ClickMap: IFrameMessage
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+        [ProtoMember(93, IsRequired = true)]
+        public long ActorId { get; set; }
 
-		[ProtoMember(94, IsRequired = true)]
-		public long Id { get; set; }
+        [ProtoMember(1)]
+        public List<UnitInfo> Units = new List<UnitInfo>();
 
-		[ProtoMember(1, IsRequired = true)]
-		public int X;
+    }
 
-		[ProtoMember(2, IsRequired = true)]
-		public int Z;
+    [Message(OuterOpcode.Frame_ClickMap)]
+    [ProtoContract]
+    public partial class Frame_ClickMap : IFrameMessage
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-	}
+        [ProtoMember(94, IsRequired = true)]
+        public long Id { get; set; }
 
-	[Message(OuterOpcode.C2M_Reload)]
-	[ProtoContract]
-	public partial class C2M_Reload: IRequest
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+        [ProtoMember(1, IsRequired = true)]
+        public int X;
 
-		[ProtoMember(1, IsRequired = true)]
-		public AppType AppType;
+        [ProtoMember(2, IsRequired = true)]
+        public int Z;
 
-	}
+    }
 
-	[Message(OuterOpcode.M2C_Reload)]
-	[ProtoContract]
-	public partial class M2C_Reload: IResponse
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    [Message(OuterOpcode.C2M_Reload)]
+    [ProtoContract]
+    public partial class C2M_Reload : IRequest
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(91, IsRequired = true)]
-		public int Error { get; set; }
+        [ProtoMember(1, IsRequired = true)]
+        public AppType AppType;
 
-		[ProtoMember(92, IsRequired = true)]
-		public string Message { get; set; }
+    }
 
-	}
+    [Message(OuterOpcode.M2C_Reload)]
+    [ProtoContract]
+    public partial class M2C_Reload : IResponse
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-	[Message(OuterOpcode.C2R_Ping)]
-	[ProtoContract]
-	public partial class C2R_Ping: IRequest
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+        [ProtoMember(91, IsRequired = true)]
+        public int Error { get; set; }
 
-	}
+        [ProtoMember(92, IsRequired = true)]
+        public string Message { get; set; }
 
-	[Message(OuterOpcode.R2C_Ping)]
-	[ProtoContract]
-	public partial class R2C_Ping: IResponse
-	{
-		[ProtoMember(90, IsRequired = true)]
-		public int RpcId { get; set; }
+    }
 
-		[ProtoMember(91, IsRequired = true)]
-		public int Error { get; set; }
+    [Message(OuterOpcode.C2R_Ping)]
+    [ProtoContract]
+    public partial class C2R_Ping : IRequest
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
 
-		[ProtoMember(92, IsRequired = true)]
-		public string Message { get; set; }
+    }
 
-	}
+    [Message(OuterOpcode.R2C_Ping)]
+    [ProtoContract]
+    public partial class R2C_Ping : IResponse
+    {
+        [ProtoMember(90, IsRequired = true)]
+        public int RpcId { get; set; }
+
+        [ProtoMember(91, IsRequired = true)]
+        public int Error { get; set; }
+
+        [ProtoMember(92, IsRequired = true)]
+        public string Message { get; set; }
+
+    }
 
 }
